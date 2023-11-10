72cf9ff2941bc847aca877e24c84f9e379df614a
thevisualriot <rydzkowska@gmail.com>
2023-11-10 20:53:43 +0000

    add event listener to the button

--- script.js
+++ script.js
@@ -3,37 +3,42 @@ var currentDay = dayjs().format('dddd, MMMM D, YYYY');
 var currentDayEl = $('#currentDay');
 var currentTime = parseInt(dayjs().format('HH'));
 currentTime = 12;
+var currentTaskCell;
+var inputTask;
+var  tasks = [];
 
 currentDayEl.text(currentDay);
 
+localStorage.setItem('9Task', '')
+
 /* TIMEBLOCKS */
 var taskCell = $('.task');
-// console.log(taskCell);
 var hourCell = $('.hour');
-// console.log(hourCell);
-
-console.log(taskCell[2])
 
 for (i=0; i<hourCell.length; i++){
     var tableTime = parseInt(hourCell[i].firstChild.data);
-    var currentTaskCell;
-
     if (tableTime === currentTime) {
-        console.log(tableTime + " it's now")
         currentTaskCell = taskCell[i];
         currentTaskCell.setAttribute('class', 'col-8 cell task present');
         currentTaskCell.disabled = true;
+
     } else if (tableTime < currentTime) {
-        console.log(tableTime + " it's in the past")
         currentTaskCell = taskCell[i];
         currentTaskCell.setAttribute('class', 'col-8 cell task past');
         currentTaskCell.disabled = true;
     } else {
-        console.log(tableTime + " it's in the future")
         currentTaskCell = taskCell[i];
         currentTaskCell.setAttribute('class', 'col-8 cell task future');
+        inputTask = taskCell.val()
+        localStorage.setItem("tasks", inputTask);
     }
     
 }
 
+/* BUTTON */
+var buttonEl = $('.saveBtn');
 
+buttonEl.on('click', function(e){
+    var clickedButton = e.target;
+    console.log(clickedButton.parentElement.id);
+})
