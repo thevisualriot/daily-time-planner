cab0c5e1ecfe3bfa43935101cb8abadab85775f7
thevisualriot <rydzkowska@gmail.com>
2023-11-11 20:02:25 +0000

    update content in LocalStorage instead of duplicating

--- script.js
+++ script.js
@@ -1,118 +1,108 @@
-/* CURRENT DATE */
-var currentDay = dayjs().format('dddd, MMMM D, YYYY');
-var currentDayEl = $('#currentDay');
-var currentTime = parseInt(dayjs().format('HH'));
+/* ------------------- VARIABLES ---------------- */
+var currentDayEl = $('#currentDay'); // access Current Day block
+var taskCell = $('.task'); // access task cells
+var hourCell = $('.hour'); // access hour cells
+var buttonEl = $('.saveBtn'); // access button
+
+var currentDay = dayjs().format('dddd, MMMM D, YYYY'); // today date
+var currentTime = parseInt(dayjs().format('HH')); // what hour it is now
 currentTime = 13;
-var currentTaskCell;
-var inputTask;
+
+// task object:
 var task = {
     id: '',
     content: '',
 }
-var  tasks = [];
-var savedTasks;
+var  tasks = []; // array of tasks
+
+currentDayEl.text(currentDay); // display today's date
+
+updateTasks();  // check if LocalStorage is not empty and update list of Tasks
 
 
-if (localStorage !== null) {
-    savedTasks = JSON.parse(localStorage.getItem('tasks'));
+/* ----------------------------- TIMEBLOCKS -------------------------------- */
+/* If anything saved to local storage, display the content in the task cells */
 
-    if (savedTasks !== null) {
-        tasks = savedTasks;
-    } else {
-        tasks = [];
-    }
-}
+for (i = 0; i < hourCell.length; i++){ 
+    var storedInput; 
+    var tableTime = parseInt(hourCell[i].firstChild.data); // store the hour as a number
+    var currentTaskCell = taskCell[i]; // select one cell
+    var cellID = currentTaskCell.parentElement.id; // store cell's parent ID (to store an info about hour)
 
-currentDayEl.text(currentDay);
-
-
-/* TIMEBLOCKS */
-var taskCell = $('.task');
-var hourCell = $('.hour');
-var content;
-var taskParentID;
-var storedInput;
-var cellID;
-
-
-for (i=0; i<hourCell.length; i++){
-    var tableTime = parseInt(hourCell[i].firstChild.data);
-
-    taskParentID = taskCell[i].parentElement.id;
-    currentTaskCell = taskCell[i];
-    cellID = currentTaskCell.parentElement.id;
-
-    if (localStorage){
-        storedInput = tasks.find(function(task) {
-            return task.id == cellID;
+    if (localStorage){                              // if localStorage is not empty
+        storedInput = tasks.find(function(task) {   // in the array of tasks, find the one that has the same ID as the current cell
+            return task.id == cellID;               // return true if so
         });
 
         if(storedInput) {
-            currentTaskCell.value = storedInput.content;
+            currentTaskCell.value = storedInput.content; // display the content of the storedInput.content in the current cell
         }
 
     } else {
-        currentTaskCell.value = '';
+        currentTaskCell.value = ''; // if empty, show nothing in the cell
     }
 
 
-    if (tableTime === currentTime) {
-        currentTaskCell.setAttribute('class', 'col-8 cell task present');
-        currentTaskCell.disabled = true;
-
+    if (tableTime === currentTime) { // if it's now
+        currentTaskCell.setAttribute('class', 'col-8 cell task present'); // change class to present
+        currentTaskCell.disabled = true; // disable input so it's impossible to type
     } else if (tableTime < currentTime) {
-        currentTaskCell.setAttribute('class', 'col-8 cell task past');
-        currentTaskCell.disabled = true;
-
+        currentTaskCell.setAttribute('class', 'col-8 cell task past'); // change class to past
+        currentTaskCell.disabled = true; // disable input so it's impossible to type
     } else {
-        currentTaskCell.setAttribute('class', 'col-8 cell task future');
-        inputTask = taskCell.val()
+        currentTaskCell.setAttribute('class', 'col-8 cell task future'); // change class to future
     }
     
 }
 
 
+/* ------------------------- SAVE BUTTON ---------------------------- */
+/* When pressed, save the input to local storage */
 
-
-
-/* ------------------ BUTTON ----------------------- */
-var buttonEl = $('.saveBtn');
-var newTask;
 
 buttonEl.on('click', function(e){
     
-    if (localStorage) {
-        savedTasks = JSON.parse(localStorage.getItem('tasks'));
-        tasks = savedTasks || [];
-    }
-
+    var inputTask; // new input
+    var newTask; // new task object
+    
+    updateTasks(); // check if LocalStorage is not empty and update list of Tasks
 
     // get input from the same row of the clicked button
-    var buttonParent = e.target.parentElement;
+    var buttonParent = e.target.parentElement; 
     buttonParent = e.target.parentElement;
-    inputTaskCell = buttonParent.querySelector('input');
-    inputTask = inputTaskCell.value;
+    inputTaskCell = buttonParent.querySelector('input'); // find the input from the same row as pressed button
+    inputTask = inputTaskCell.value; //update inputTask with the value
 
     // add input to tasks
-    if (inputTask !== '') {
-        newTask = Object.create(task);
+        newTask = Object.create(task);  // create new task object
         newTask.id = buttonParent.id;
         newTask.content = inputTask;
         saveToLocalStorage(newTask);
-    } 
 })
 
 
 function saveToLocalStorage(object){
-    tasks.push(object);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+    var duplicate = tasks.findIndex(task => task.id === object.id);
 
-function updateValueFromLocalStorage(){
-    if (localStorage != null){
-        savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if(duplicate !== -1) {
+        tasks[duplicate].content = object.content;
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     } else {
-        return;
+        tasks.push(object);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 }
 
+
+function updateTasks() {
+    var savedTasks;
+    if (localStorage) {
+        savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    
+        if (savedTasks) {
+            tasks = savedTasks;
+        } else {
+            tasks = [];
+        }
+    }
+}
